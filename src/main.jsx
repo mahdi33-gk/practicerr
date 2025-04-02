1acc16e3e3f3015935b7e6396e3e9c74991f1086
mehedi <mehedi25122003@gmail.com>
2025-04-02 10:43:49 +0600

    failed to connnet with google map

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,14 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.jsx";
+import { APIProvider } from "@vis.gl/react-google-maps";
+import { googleApi } from "./googleApi.js";
 
-createRoot(document.getElementById('root')).render(
+createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
-)
+    <APIProvider apiKey={googleApi}>
+      <App />
+    </APIProvider>
+  </StrictMode>
+);
